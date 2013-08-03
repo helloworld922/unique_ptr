ad8687e4ef0a4a72e3959d1f4ce6c0c2f0277df9
helloworld922 <helloworld922@gmail.com>
2013-08-02 19:08:47 -0600

    added make_unique.hpp

---
+++ unique_ptr/boost/make_unique.hpp
@@ -0,0 +1,19 @@
+//
+// make_unique.hpp
+//
+// (c) 2013 helloworld922
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MAKE_UNIQUE_HPP_
+#define MAKE_UNIQUE_HPP_
+
+namespace boost
+{
+
+}
+
+
+#endif // MAKE_UNIQUE_HPP_
